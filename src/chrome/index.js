dc8d8ae72a816ddf8af2319bdcd94730fe80d9fa
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-24 17:49:58 +1200

    add chrome implementation

---
+++ src/chrome/index.js
@@ -0,0 +1,111 @@
+function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === "complete"
+      : document.readyState !== "loading"
+  ) {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function waitForFB(watch, callback) {
+  // observe when post finish loading
+  const observer = new MutationObserver(mutationsList => {
+    mutationsList.forEach(mutation => {
+      // the "async_saving" class indicate loading post
+      if (
+        mutation.type === "attributes" &&
+        !watch.classList.contains("async_saving")
+      ) {
+        // if post finish loading, then disconnect the observer and call the callback
+        observer.disconnect();
+        callback();
+      }
+    });
+  });
+  const config = { attributes: true, childList: false, subtree: false };
+  observer.observe(watch, config);
+}
+
+function wpm(wordCount) {
+  const totalSeconds = wordCount / 3;
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = Math.floor(totalSeconds - minutes * 60);
+  return { minutes: minutes, seconds: seconds };
+}
+
+function addReadingTime(target) {
+  const posts = target.getElementsByClassName("text_exposed_root");
+  if (posts === undefined || posts === null || posts.length === 0) {
+    return;
+  }
+  console.log(posts);
+  for (o of posts) {
+    const wordCount = Array.from(o.getElementsByTagName("p")).reduce(
+      (accumulator, currentValue) => {
+        return accumulator + currentValue.innerText.split(" ").length;
+      },
+      0
+    );
+    const time = wpm(wordCount);
+    o.getElementsByClassName(
+      "see_more_link_inner"
+    )[0].innerText = `...estimated reading time ${time.minutes}m ${
+      time.seconds
+    }s`;
+  }
+}
+
+function observeMainPage() {
+  // get the post stream
+  const feed = document.querySelector("[role='feed']");
+  const postStream = feed.lastChild.firstChild;
+  if (postStream === undefined || postStream === null) {
+    throw "couldn't get feed";
+  }
+  console.log(postStream);
+  addReadingTime(feed);
+  // get fb placeholder for loading post
+  const asyncWatch = feed.lastChild.lastChild;
+  // set up an observer which detect newly added posts
+  const observer = new MutationObserver(mutationsList =>
+    mutationsList.forEach(mutation => {
+      // call waitForFB on add posts, pass in the loading placeholder and a callback
+      waitForFB(asyncWatch, () => addReadingTime(mutation.addedNodes[0]));
+    })
+  );
+  const config = { attributes: false, childList: true, subtree: false };
+  observer.observe(postStream, config);
+  return observer;
+}
+
+function main() {
+  let mainpageObserver;
+  let success = false;
+  // try setting up an observer every 1/2 sec
+  function tryBlock() {
+    try {
+      // if found call observeMainPage() which will return an observer
+      mainpageObserver = observeMainPage();
+      success = true;
+    } catch (e) {
+      success = false;
+      setTimeout(main, 500);
+    }
+  }
+  tryBlock();
+  console.log(mainpageObserver);
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // listen for messages sent from background.js
+    if (request.message === "urlChange" && success === true) {
+      console.log("change");
+      success = false;
+      mainpageObserver.disconnect();
+      tryBlock();
+    }
+  });
+}
+
+ready(main);
